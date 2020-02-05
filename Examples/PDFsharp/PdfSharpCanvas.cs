0c8cdff70ef09d543621061cecc5fe0971d9c5ea
Robert Hegner (Laptop) <rhegner@hsr.ch>
2020-02-06 00:13:36 +0100

    Custom ICanvas implementation for PDFsharp. Not production ready yet. Issues:
    - One of the scissors not drawn correctly.
    - Cross missing in QR code
    - All lines in a path are connected

---
+++ Examples/PDFsharp/PdfSharpCanvas.cs
@@ -0,0 +1,116 @@
+﻿using Codecrete.SwissQRBill.Generator.Canvas;
+using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+
+namespace PDFsharp
+{
+    class PdfSharpCanvas : AbstractCanvas
+    {
+
+        private readonly XGraphics XGraphics;
+        private readonly string FontFamily;
+
+        private XGraphicsPath CurrentPath;
+        private XPoint CurrentPathPoint;
+
+        public PdfSharpCanvas(PdfPage page, string fontFamily)
+        {
+            FontFamily = fontFamily;
+            SetupFontMetrics(FontFamily);
+
+            XGraphics = XGraphics.FromPdfPage(page, XGraphicsPdfPageOptions.Append, XGraphicsUnit.Millimeter, XPageDirection.Downwards);
+
+            // The origin of an XGraphics canvas is in the top left corner, with the y axis pointing downwards.
+            // This transform mirrors the y axis. Unfortunately this also mirrors text output (see workaround in PutText).
+            // XGraphics.FromPdfPage can be called with XPageDirection.Upwards, which is marked as obsolete because it is not implemented properly
+            // it suffers from the same problem as our matrix below.
+            XGraphics.MultiplyTransform(new XMatrix(1.0, 0.0, 0.0, -1.0, 0.0, XGraphics.PageSize.Height));
+
+            // Save the current state so that we can always revert any transformations we apply.
+            XGraphics.Save();
+        }
+
+        public override void SetTransformation(double translateX, double translateY, double rotate, double scaleX, double scaleY)
+        {
+            // Revert to original state, and immediately put this state on the stack again.
+            XGraphics.Restore();
+            XGraphics.Save();
+
+            XGraphics.TranslateTransform(translateX, translateY);
+            if (rotate != 0.0)
+                XGraphics.RotateTransform(rotate);
+            if (scaleX != 1.0 || scaleY != 1.0)
+                XGraphics.ScaleTransform(scaleX, scaleY);           
+        }
+
+        public override void PutText(string text, double x, double y, int fontSize, bool isBold)
+        {
+            var font = new XFont(FontFamily, XUnit.FromPoint(fontSize).Millimeter, isBold ? XFontStyle.Bold : XFontStyle.Regular);
+
+            // With our transformation to mirror the y axis, text printed using DrawString also gets mirrored.
+            // As a workaround we move our current transformation to the x/y position and then mirror the y axis again, and then restore the previous transformation.
+            XGraphics.Save();
+            XGraphics.TranslateTransform(x, y);
+            XGraphics.MultiplyTransform(new XMatrix(1.0, 0.0, 0.0, -1.0, 0.0, 0.0));
+            XGraphics.DrawString(text, font, XBrushes.Black, 0.0, 0.0);
+            XGraphics.Restore();
+        }
+
+        public override void StartPath()
+        {
+            CurrentPath = new XGraphicsPath();
+            CurrentPathPoint = new XPoint(0.0, 0.0);
+        }
+
+        public override void MoveTo(double x, double y)
+        {
+            CurrentPathPoint = new XPoint(x, y);
+        }
+
+        public override void LineTo(double x, double y)
+        {
+            var point = new XPoint(x, y);
+            CurrentPath.AddLine(CurrentPathPoint, point);
+            CurrentPathPoint = point;
+        }
+
+        public override void CubicCurveTo(double x1, double y1, double x2, double y2, double x, double y)
+        {
+            var controlPoint1 = new XPoint(x1, y1);
+            var controlPoint2 = new XPoint(x2, y2);
+            var point = new XPoint(x, y);
+            CurrentPath.AddBezier(CurrentPathPoint, controlPoint1, controlPoint2, point);
+            CurrentPathPoint = point;
+        }
+
+        public override void AddRectangle(double x, double y, double width, double height)
+        {
+            // TODO: Do we need to update CurrentPathPoint here?
+            CurrentPath.AddRectangle(x, y, width, height);
+        }
+
+        public override void CloseSubpath()
+        {
+            // TODO: Anything we need to do here?
+        }
+
+        public override void FillPath(int color)
+        {
+            // TODO: Use color (is it really necessary??)
+            XGraphics.DrawPath(XBrushes.Black, CurrentPath);
+        }
+
+        public override void StrokePath(double strokeWidth, int color)
+        {
+            // TODO: Use color (is it really necessary??)
+            var pen = new XPen(XColors.Black, XUnit.FromPoint(strokeWidth).Millimeter);
+            XGraphics.DrawPath(pen, CurrentPath);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+                XGraphics.Dispose();
+        }
+    }
+}
---
+++ Examples/PDFsharp/Program.cs
@@ -0,0 +1,58 @@
+﻿using Codecrete.SwissQRBill.Generator;
+using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+using System.IO;
+
+namespace PDFsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var doc = new PdfDocument())
+            {
+                var page = doc.AddPage();
+                using (var graphic = XGraphics.FromPdfPage(page, XGraphicsUnit.Millimeter))
+                {
+                    graphic.DrawString("Test Invoice", new XFont("Arial", 20.0), XBrushes.Black, 20.0, 150.0);
+                }
+                using (var canvas = new PdfSharpCanvas(page, "Arial"))
+                {
+                    QRBill.Draw(Bill, canvas);
+                }
+                doc.Save("Output.pdf");
+            }
+
+            File.WriteAllBytes("OutputOriginal.pdf", QRBill.Generate(Bill));
+        }
+
+        static readonly Bill Bill = new Bill
+		{
+			// creditor data
+			Account = "CH9600781622484102000",
+			Creditor = new Address
+			{
+				Name = "Seemannschaft Rapperswil",
+				AddressLine2 = "8640 Rapperswil",
+				CountryCode = "CH"
+			},
+
+			// payment data
+			Amount = null,
+			Currency = "CHF",
+
+			// more payment data
+			UnstructuredMessage = "Spende",
+
+            // format
+            Format = new BillFormat
+            {
+                 Language = Language.DE,
+                 SeparatorType = SeparatorType.SolidLineWithScissors,
+                 OutputSize = OutputSize.A4PortraitSheet,
+                 GraphicsFormat = GraphicsFormat.PDF,
+                 FontFamily = "Arial"
+            }
+		};
+	}
+}
