82ab770df57371a3b4587c293e875c6bb80b68f7
manuelbl <10954524+manuelbl@users.noreply.github.com>
2021-11-30 22:20:21 +0100

    Windows Forms example (display and printing)

---
+++ Examples/WindowsForms/Form1.cs
@@ -0,0 +1,94 @@
+﻿//
+// Swiss QR Bill Generator for .NET
+// Copyright (c) 2021 Manuel Bleichenbacher
+// Licensed under MIT License
+// https://opensource.org/licenses/MIT
+//
+
+using Codecrete.SwissQRBill.Generator;
+using System;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Windows.Forms;
+
+namespace Codecrete.SwissQRBill.Examples.WindowsForms
+{
+    public partial class Form1 : Form
+    {
+        Bill bill;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // create bill data
+            bill = new Bill
+            {
+                // creditor data
+                Account = "CH4431999123000889012",
+                Creditor = new Address
+                {
+                    Name = "Robert Schneider AG",
+                    AddressLine1 = "Rue du Lac 1268/2/22",
+                    AddressLine2 = "2501 Biel",
+                    CountryCode = "CH"
+                },
+
+                // payment data
+                Amount = 199.95m,
+                Currency = "CHF",
+
+                // debtor data
+                Debtor = new Address
+                {
+                    Name = "Pia-Maria Rutschmann-Schnyder",
+                    AddressLine1 = "Grosse Marktgasse 28",
+                    AddressLine2 = "9400 Rorschach",
+                    CountryCode = "CH"
+                },
+
+                // more payment data
+                Reference = "210000000003139471430009017",
+                UnstructuredMessage = "Abonnement für 2020"
+            };
+
+            // set bill data for QR bill control
+            qrBillControl1.Bill = bill;
+        }
+
+        private void printButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                PrintDocument pd = new PrintDocument();
+                pd.PrintPage += new PrintPageEventHandler(printDocument_PrintPage);
+                pd.Print();
+                MessageBox.Show("QR bill successfully printed");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void printDocument_PrintPage(object sender, PrintPageEventArgs e)
+        {
+            using Font font = new Font("Arial", 18, FontStyle.Bold);
+            e.Graphics.DrawString("Swiss QR Bill", font, Brushes.Black, 30, 80, new StringFormat());
+
+            PrintQRBill(e);
+        }
+
+        private void PrintQRBill(PrintPageEventArgs e)
+        {
+            // ouput size for QR bill: 210 x 297mm
+            bill.Format.OutputSize = OutputSize.A4PortraitSheet;
+
+            // print QR bill at bottom of page
+            Rectangle bounds = e.PageBounds;
+            float scale = bounds.Width / 210f; // scale to full width
+            using SystemDrawingCanvas canvas = new SystemDrawingCanvas(e.Graphics, 0, 297 * scale, scale, "Arial");
+            QRBill.Draw(bill, canvas);
+        }
+    }
+}
+
---
+++ Examples/WindowsForms/Program.cs
@@ -0,0 +1,27 @@
+//
+// Swiss QR Bill Generator for .NET
+// Copyright (c) 2021 Manuel Bleichenbacher
+// Licensed under MIT License
+// https://opensource.org/licenses/MIT
+//
+
+using System;
+using System.Windows.Forms;
+
+namespace Codecrete.SwissQRBill.Examples.WindowsForms
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
